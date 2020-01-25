6f3c2b326b8af9f93291f2b4073752223d0f8617
Hessam <hessam.modaberi@gmail.com>
2020-01-26 01:40:27 +0330

    feat: removes faceted search processing

--- controllers/front/categoryproducts.php
+++ controllers/front/categoryproducts.php
@@ -36,6 +36,7 @@ class BinshopsrestCategoryproductsModuleFrontController extends AbstractRESTProd
             'sort_orders' => $variables['sort_orders'],
             'sort_selected' => $variables['sort_selected'],
             'pagination' => $variables['pagination']
+            //todo: handle faceted_search
         ];
 
         $this->ajaxRender(json_encode([
@@ -123,7 +124,168 @@ class BinshopsrestCategoryproductsModuleFrontController extends AbstractRESTProd
 
     protected function getProductSearchVariables()
     {
-        //todo
-        return parent::getProductSearchVariables();
+        /*
+         * To render the page we need to find something (a ProductSearchProviderInterface)
+         * that knows how to query products.
+         */
+
+        // the search provider will need a context (language, shop...) to do its job
+        $context = $this->getProductSearchContext();
+
+        // the controller generates the query...
+        $query = $this->getProductSearchQuery();
+
+        // ...modules decide if they can handle it (first one that can is used)
+        $provider = $this->getProductSearchProviderFromModules($query);
+
+        // if no module wants to do the query, then the core feature is used
+        if (null === $provider) {
+            $provider = $this->getDefaultProductSearchProvider();
+        }
+
+        $resultsPerPage = (int) Tools::getValue('resultsPerPage');
+        if ($resultsPerPage <= 0) {
+            $resultsPerPage = Configuration::get('PS_PRODUCTS_PER_PAGE');
+        }
+
+        // we need to set a few parameters from back-end preferences
+        $query
+            ->setResultsPerPage($resultsPerPage)
+            ->setPage(max((int) Tools::getValue('page'), 1))
+        ;
+
+        // set the sort order if provided in the URL
+        if (($encodedSortOrder = Tools::getValue('order'))) {
+            $query->setSortOrder(SortOrder::newFromString(
+                $encodedSortOrder
+            ));
+        }
+
+        // get the parameters containing the encoded facets from the URL
+        $encodedFacets = Tools::getValue('q');
+
+        /*
+         * The controller is agnostic of facets.
+         * It's up to the search module to use /define them.
+         *
+         * Facets are encoded in the "q" URL parameter, which is passed
+         * to the search provider through the query's "$encodedFacets" property.
+         */
+
+        $query->setEncodedFacets($encodedFacets);
+
+        // We're ready to run the actual query!
+
+        /** @var ProductSearchResult $result */
+        $result = $provider->runQuery(
+            $context,
+            $query
+        );
+
+        if (Configuration::get('PS_CATALOG_MODE') && !Configuration::get('PS_CATALOG_MODE_WITH_PRICES')) {
+            $this->disablePriceControls($result);
+        }
+
+        // sort order is useful for template,
+        // add it if undefined - it should be the same one
+        // as for the query anyway
+        if (!$result->getCurrentSortOrder()) {
+            $result->setCurrentSortOrder($query->getSortOrder());
+        }
+
+        // prepare the products
+        $products = $this->prepareMultipleProductsForTemplate(
+            $result->getProducts()
+        );
+
+        // render the facets
+        //todo: commented by HESSAM
+//        if ($provider instanceof FacetsRendererInterface) {
+//            // with the provider if it wants to
+//            $rendered_facets = $provider->renderFacets(
+//                $context,
+//                $result
+//            );
+//            $rendered_active_filters = $provider->renderActiveFilters(
+//                $context,
+//                $result
+//            );
+//        } else {
+//            // with the core
+//            $rendered_facets = $this->renderFacets(
+//                $result
+//            );
+//            $rendered_active_filters = $this->renderActiveFilters(
+//                $result
+//            );
+//        }
+
+        $pagination = $this->getTemplateVarPagination(
+            $query,
+            $result
+        );
+
+        // prepare the sort orders
+        // note that, again, the product controller is sort-orders
+        // agnostic
+        // a module can easily add specific sort orders that it needs
+        // to support (e.g. sort by "energy efficiency")
+        $sort_orders = $this->getTemplateVarSortOrders(
+            $result->getAvailableSortOrders(),
+            $query->getSortOrder()->toString()
+        );
+
+        $sort_selected = false;
+        if (!empty($sort_orders)) {
+            foreach ($sort_orders as $order) {
+                if (isset($order['current']) && true === $order['current']) {
+                    $sort_selected = $order['label'];
+
+                    break;
+                }
+            }
+        }
+
+        $searchVariables = array(
+            'result' => $result,
+            'label' => $this->getListingLabel(),
+            'products' => $products,
+            'sort_orders' => $sort_orders,
+            'sort_selected' => $sort_selected,
+            'pagination' => $pagination,
+//todo: commented by HESSAM
+
+//            'rendered_facets' => $rendered_facets,
+//            'rendered_active_filters' => $rendered_active_filters,
+            'js_enabled' => $this->ajax,
+            'current_url' => $this->updateQueryString(array(
+                'q' => $result->getEncodedFacets(),
+            )),
+        );
+
+        Hook::exec('filterProductSearch', array('searchVariables' => &$searchVariables));
+        Hook::exec('actionProductSearchAfter', $searchVariables);
+
+        return $searchVariables;
+    }
+
+    private function getProductSearchProviderFromModules($query)
+    {
+        $providers = Hook::exec(
+            'productSearchProvider',
+            array('query' => $query),
+            null,
+            true
+        );
+
+        if (!is_array($providers)) {
+            $providers = array();
+        }
+
+        foreach ($providers as $provider) {
+            if ($provider instanceof ProductSearchProviderInterface) {
+                return $provider;
+            }
+        }
     }
 }
