1c8c9b94ef824a9b93a58e5f2d8f13d9e6585589
Hessam <hessam.modaberi@gmail.com>
2020-04-13 23:16:10 +0430

    fixes banner image

--- controllers/front/bootstrap.php
+++ controllers/front/bootstrap.php
@@ -7,17 +7,17 @@ require_once __DIR__ . '/../AbstractRESTController.php';
 
 class BinshopsrestBootstrapModuleFrontController extends AbstractRESTController
 {
+    protected $banner;
     protected function processGetRequest()
     {
         $messageCode = 200;
         $mainMenu = Module::getInstanceByName('ps_mainmenu');
         $featuredProducts = Module::getInstanceByName('ps_featuredproducts');
-        $banner = Module::getInstanceByName('ps_banner');
+        $this->banner = Module::getInstanceByName('ps_banner');
         $imagesSlider = Module::getInstanceByName('ps_imageslider');
 
         $menuItems = $mainMenu->getWidgetVariables(null, []);
         $featuredProductsList = $featuredProducts->getWidgetVariables(null, []);
-        $bannerItem = $banner->getWidgetVariables(null, []);
         $slidesList = $imagesSlider->getWidgetVariables(null, []);
         $menuItems = $menuItems['children'];
         $retriever = new \PrestaShop\PrestaShop\Adapter\Image\ImageRetriever(
@@ -37,7 +37,7 @@ class BinshopsrestBootstrapModuleFrontController extends AbstractRESTController
         $psdata = array();
         $psdata['menuItems'] = $menuItems;
         $psdata['featuredProductsList'] = $featuredProductsList['products'];
-        $psdata['banner'] = $bannerItem;
+        $psdata['banner'] = $this->getBanner();
         $psdata['slides'] = $slidesList['homeslider']['slides'];
 
         $this->ajaxRender(json_encode([
@@ -74,4 +74,32 @@ class BinshopsrestBootstrapModuleFrontController extends AbstractRESTController
         die;
     }
 
+    protected function getBanner(){
+        $imgname = Configuration::get('BANNER_IMG', $this->context->language->id);
+        $image_url = "";
+
+        if ($imgname && file_exists(_PS_MODULE_DIR_.$this->banner->name.DIRECTORY_SEPARATOR.'img'.DIRECTORY_SEPARATOR.$imgname)) {
+            $image_url = $this->context->link->protocol_content . Tools::getMediaServer($imgname) . __PS_BASE_URI__ . 'modules/'. $this->banner->name . '/' . 'img/' . $imgname;
+        }
+
+        $banner_link = Configuration::get('BANNER_LINK', $this->context->language->id);
+        if (!$banner_link) {
+            $banner_link = $this->context->link->getPageLink('index');
+        }
+
+        return array(
+            'image_url' => $image_url,
+            'banner_link' => $this->updateUrl($banner_link),
+            'banner_desc' => Configuration::get('BANNER_DESC', $this->context->language->id)
+        );
+    }
+
+    private function updateUrl($link)
+    {
+        if (substr($link, 0, 7) !== "http://" && substr($link, 0, 8) !== "https://") {
+            $link = "http://" . $link;
+        }
+
+        return $link;
+    }
 }
