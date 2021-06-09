304344379aae64ef8c4b4db62740fc0a2e22b4d7
Sam <sam@binshops.com>
2021-06-08 20:16:15 -0500

    populates all variables related to products

--- controllers/front/productsearch.php
+++ controllers/front/productsearch.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../AbstractProductListingRESTController.php';
+require_once __DIR__ . '/../../classes/RESTProductLazyArray.php';
 define('PRICE_REDUCTION_TYPE_PERCENT' , 'percentage');
 
 use PrestaShop\PrestaShop\Adapter\Search\SearchProductSearchProvider;
@@ -36,8 +37,22 @@ class BinshopsrestProductsearchModuleFrontController extends AbstractProductList
             $this->context->link
         );
 
+        $settings = $this->getProductPresentationSettings();
+
         foreach ($productList as $key => $product){
-            $productList[$key]['images'] = $retriever->getProductImages($product, $this->context->language);
+            $populated_product = (new ProductAssembler($this->context))
+                ->assembleProduct($product);
+
+            $lazy_product = new RESTProductLazyArray(
+                $settings,
+                $populated_product,
+                $this->context->language,
+                new \PrestaShop\PrestaShop\Adapter\Product\PriceFormatter(),
+                $retriever,
+                $this->context->getTranslator()
+            );
+
+            $productList[$key]['prod_info'] = $lazy_product->getProduct();
         }
 
         if($variables['facets']){
