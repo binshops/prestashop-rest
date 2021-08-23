43153d2bb5ee27f58e7c3771ca808631521a3f61
Sam <sam@binshops.com>
2021-08-23 16:22:31 -0500

    optimization in bootstrap endpoint plus adding paroduct lazy array

--- controllers/front/bootstrap.php
+++ controllers/front/bootstrap.php
@@ -8,6 +8,12 @@
  */
 
 require_once dirname(__FILE__) . '/../AbstractRESTController.php';
+require_once dirname(__FILE__) . '/../../classes/RESTProductLazyArray.php';
+
+use PrestaShop\PrestaShop\Adapter\Category\CategoryProductSearchProvider;
+use PrestaShop\PrestaShop\Core\Product\Search\ProductSearchContext;
+use PrestaShop\PrestaShop\Core\Product\Search\ProductSearchQuery;
+use PrestaShop\PrestaShop\Core\Product\Search\SortOrder;
 
 /**
  * Description: This class bootstraps the main page of the application
@@ -21,12 +27,11 @@ class BinshopsrestBootstrapModuleFrontController extends AbstractRESTController
     {
         $messageCode = 200;
         $mainMenu = Module::getInstanceByName('ps_mainmenu');
-        $featuredProducts = Module::getInstanceByName('ps_featuredproducts');
         $this->banner = Module::getInstanceByName('ps_banner');
         $imagesSlider = Module::getInstanceByName('ps_imageslider');
 
         $menuItems = $mainMenu->getWidgetVariables(null, []);
-        $featuredProductsList = $featuredProducts->getWidgetVariables(null, []);
+        $featuredProductsList = $this->getFeaturedProducts();
         $slidesList = $imagesSlider->getWidgetVariables(null, []);
         $menuItems = $menuItems['children'];
 
@@ -117,5 +122,83 @@ class BinshopsrestBootstrapModuleFrontController extends AbstractRESTController
         }
 
         return $link;
+    }
+
+    public function getFeaturedProducts(){
+        $category = new Category((int) Configuration::get('HOME_FEATURED_CAT'));
+
+        $searchProvider = new CategoryProductSearchProvider(
+            $this->context->getTranslator(),
+            $category
+        );
+
+        $context = new ProductSearchContext($this->context);
+
+        $query = new ProductSearchQuery();
+        $nProducts = Configuration::get('HOME_FEATURED_NBR');
+        if ($nProducts < 0) {
+            $nProducts = 12;
+        }
+
+        $query
+            ->setResultsPerPage($nProducts)
+            ->setPage(1)
+        ;
+
+        if (Configuration::get('HOME_FEATURED_RANDOMIZE')) {
+            $query->setSortOrder(SortOrder::random());
+        } else {
+            $query->setSortOrder(new SortOrder('product', 'position', 'asc'));
+        }
+
+        $result = $searchProvider->runQuery(
+            $context,
+            $query
+        );
+
+        $products_for_template = [];
+        $settings = $this->getProductPresentationSettings();
+        $retriever = new \PrestaShop\PrestaShop\Adapter\Image\ImageRetriever(
+            $this->context->link
+        );
+
+        foreach ($result->getProducts() as $rawProduct) {
+            $populated_product = (new ProductAssembler($this->context))
+                ->assembleProduct($rawProduct);
+            $lazy_product = new RESTProductLazyArray(
+                $settings,
+                $populated_product,
+                $this->context->language,
+                new \PrestaShop\PrestaShop\Adapter\Product\PriceFormatter(),
+                $retriever,
+                $this->context->getTranslator()
+            );
+
+            $products_for_template[] = $lazy_product->getProduct();
+        }
+
+        return array(
+            'products' => $products_for_template,
+            'allProductsLink' => Context::getContext()->link->getCategoryLink($this->getConfigFieldsValues()['HOME_FEATURED_CAT']),
+        );
+    }
+
+    public function getConfigFieldsValues()
+    {
+        return array(
+            'HOME_FEATURED_NBR' => Tools::getValue('HOME_FEATURED_NBR', (int) Configuration::get('HOME_FEATURED_NBR')),
+            'HOME_FEATURED_CAT' => Tools::getValue('HOME_FEATURED_CAT', (int) Configuration::get('HOME_FEATURED_CAT')),
+            'HOME_FEATURED_RANDOMIZE' => Tools::getValue('HOME_FEATURED_RANDOMIZE', (bool) Configuration::get('HOME_FEATURED_RANDOMIZE')),
+        );
+    }
+
+    private function getFactory()
+    {
+        return new ProductPresenterFactory($this->context, new TaxConfiguration());
+    }
+
+    protected function getProductPresentationSettings()
+    {
+        return $this->getFactory()->getPresentationSettings();
     }
 }
