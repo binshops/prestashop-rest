a51d85a623ad5e1f3eeedc765190f4e43ef121ea
Sam <sam@binshops.com>
2021-08-24 05:46:46 -0500

    version upgrade

--- Readme.md
+++ Readme.md
@@ -6,3 +6,6 @@ Webservices feature to be enabled. Totally free without any restriction.
 ### API Endpoint
 
 Your REST API endpoint: http://yourdomain.tld/rest.
+
+### Change Log
+- #### 2.2.0 improves bootstrap api - adds id and slug to menu items 
--- binshopsrest.php
+++ binshopsrest.php
@@ -19,7 +19,7 @@ class Binshopsrest extends Module
     {
         $this->name = 'binshopsrest';
         $this->tab = 'others';
-        $this->version = '2.1.4';
+        $this->version = '2.2.0';
         $this->author = 'Binshops';
         $this->need_instance = 0;
 
