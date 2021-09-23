e7f17b4052337c7a111b451ca7cb060c36350e7f
Sam <sam@binshops.com>
2021-09-23 06:23:23 -0500

    version update

--- binshopsrest.php
+++ binshopsrest.php
@@ -21,7 +21,7 @@ class Binshopsrest extends Module
     {
         $this->name = 'binshopsrest';
         $this->tab = 'others';
-        $this->version = '2.2.1';
+        $this->version = '2.2.2';
         $this->author = 'Binshops';
         $this->need_instance = 0;
 
