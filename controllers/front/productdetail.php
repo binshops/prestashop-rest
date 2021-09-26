7ddab03a2fccf03c2d9ea1471265f39df6f3fa0e
Sam <sam@binshops.com>
2021-09-26 09:34:32 -0500

    adds groups to product detail api

--- controllers/front/productdetail.php
+++ controllers/front/productdetail.php
@@ -58,6 +58,7 @@ class BinshopsrestProductdetailModuleFrontController extends AbstractRESTControl
             //this is when you change an attribute, every time a request is sent to get the price and its discount
             if ((boolean)Tools::getValue('refresh', 0)) {
                 $product = $this->getTemplateVarProduct();
+                $product['groups'] = $this->assignAttributesGroups($product);
 
                 $this->ajaxRender(json_encode([
                     'psdata' => $product,
@@ -67,10 +68,13 @@ class BinshopsrestProductdetailModuleFrontController extends AbstractRESTControl
                 die;
             }
 
+            $product = $this->getProduct();
+            $product['groups'] = $this->assignAttributesGroups($product);
+
             $this->ajaxRender(json_encode([
                 'success' => true,
                 'code' => 200,
-                'psdata' => $this->getProduct()
+                'psdata' => $product
             ]));
             die;
         }
@@ -818,5 +822,210 @@ class BinshopsrestProductdetailModuleFrontController extends AbstractRESTControl
         $settings->lastRemainingItems = Configuration::get('PS_LAST_QTIES');
 
         return $settings;
+    }
+
+    protected function assignAttributesGroups($product_for_template = null)
+    {
+        $colors = [];
+        $groups = [];
+        $this->combinations = [];
+
+        /** @todo (RM) should only get groups and not all declination ? */
+        $attributes_groups = $this->product->getAttributesGroups($this->context->language->id);
+        if (is_array($attributes_groups) && $attributes_groups) {
+            $combination_images = $this->product->getCombinationImages($this->context->language->id);
+            $combination_prices_set = [];
+            foreach ($attributes_groups as $k => $row) {
+                // Color management
+                if (isset($row['is_color_group']) && $row['is_color_group'] && (isset($row['attribute_color']) && $row['attribute_color']) || (file_exists(_PS_COL_IMG_DIR_ . $row['id_attribute'] . '.jpg'))) {
+                    $colors[$row['id_attribute']]['value'] = $row['attribute_color'];
+                    $colors[$row['id_attribute']]['name'] = $row['attribute_name'];
+                    if (!isset($colors[$row['id_attribute']]['attributes_quantity'])) {
+                        $colors[$row['id_attribute']]['attributes_quantity'] = 0;
+                    }
+                    $colors[$row['id_attribute']]['attributes_quantity'] += (int) $row['quantity'];
+                }
+                if (!isset($groups[$row['id_attribute_group']])) {
+                    $groups[$row['id_attribute_group']] = [
+                        'group_name' => $row['group_name'],
+                        'name' => $row['public_group_name'],
+                        'group_type' => $row['group_type'],
+                        'default' => -1,
+                    ];
+                }
+
+                $groups[$row['id_attribute_group']]['attributes'][$row['id_attribute']] = [
+                    'name' => $row['attribute_name'],
+                    'html_color_code' => $row['attribute_color'],
+                    'texture' => (@filemtime(_PS_COL_IMG_DIR_ . $row['id_attribute'] . '.jpg')) ? _THEME_COL_DIR_ . $row['id_attribute'] . '.jpg' : '',
+                    'selected' => (isset($product_for_template['attributes'][$row['id_attribute_group']]['id_attribute']) && $product_for_template['attributes'][$row['id_attribute_group']]['id_attribute'] == $row['id_attribute']) ? true : false,
+                ];
+
+                //$product.attributes.$id_attribute_group.id_attribute eq $id_attribute
+                if ($row['default_on'] && $groups[$row['id_attribute_group']]['default'] == -1) {
+                    $groups[$row['id_attribute_group']]['default'] = (int) $row['id_attribute'];
+                }
+                if (!isset($groups[$row['id_attribute_group']]['attributes_quantity'][$row['id_attribute']])) {
+                    $groups[$row['id_attribute_group']]['attributes_quantity'][$row['id_attribute']] = 0;
+                }
+                $groups[$row['id_attribute_group']]['attributes_quantity'][$row['id_attribute']] += (int) $row['quantity'];
+
+                $this->combinations[$row['id_product_attribute']]['attributes_values'][$row['id_attribute_group']] = $row['attribute_name'];
+                $this->combinations[$row['id_product_attribute']]['attributes'][] = (int) $row['id_attribute'];
+                $this->combinations[$row['id_product_attribute']]['price'] = (float) $row['price'];
+
+                // Call getPriceStatic in order to set $combination_specific_price
+                if (!isset($combination_prices_set[(int) $row['id_product_attribute']])) {
+                    $combination_specific_price = null;
+                    Product::getPriceStatic((int) $this->product->id, false, $row['id_product_attribute'], 6, null, false, true, 1, false, null, null, null, $combination_specific_price);
+                    $combination_prices_set[(int) $row['id_product_attribute']] = true;
+                    $this->combinations[$row['id_product_attribute']]['specific_price'] = $combination_specific_price;
+                }
+                $this->combinations[$row['id_product_attribute']]['ecotax'] = (float) $row['ecotax'];
+                $this->combinations[$row['id_product_attribute']]['weight'] = (float) $row['weight'];
+                $this->combinations[$row['id_product_attribute']]['quantity'] = (int) $row['quantity'];
+                $this->combinations[$row['id_product_attribute']]['reference'] = $row['reference'];
+                $this->combinations[$row['id_product_attribute']]['unit_impact'] = $row['unit_price_impact'];
+                $this->combinations[$row['id_product_attribute']]['minimal_quantity'] = $row['minimal_quantity'];
+                if ($row['available_date'] != '0000-00-00' && Validate::isDate($row['available_date'])) {
+                    $this->combinations[$row['id_product_attribute']]['available_date'] = $row['available_date'];
+                    $this->combinations[$row['id_product_attribute']]['date_formatted'] = Tools::displayDate($row['available_date']);
+                } else {
+                    $this->combinations[$row['id_product_attribute']]['available_date'] = $this->combinations[$row['id_product_attribute']]['date_formatted'] = '';
+                }
+
+                if (!isset($combination_images[$row['id_product_attribute']][0]['id_image'])) {
+                    $this->combinations[$row['id_product_attribute']]['id_image'] = -1;
+                } else {
+                    $this->combinations[$row['id_product_attribute']]['id_image'] = $id_image = (int) $combination_images[$row['id_product_attribute']][0]['id_image'];
+                    if ($row['default_on']) {
+                        foreach ($this->context->smarty->tpl_vars['product']->value['images'] as $image) {
+                            if ($image['cover'] == 1) {
+                                $current_cover = $image;
+                            }
+                        }
+                        if (!isset($current_cover)) {
+                            $current_cover = array_values($this->context->smarty->tpl_vars['product']->value['images'])[0];
+                        }
+
+                        if (is_array($combination_images[$row['id_product_attribute']])) {
+                            foreach ($combination_images[$row['id_product_attribute']] as $tmp) {
+                                if ($tmp['id_image'] == $current_cover['id_image']) {
+                                    $this->combinations[$row['id_product_attribute']]['id_image'] = $id_image = (int) $tmp['id_image'];
+
+                                    break;
+                                }
+                            }
+                        }
+
+                        if ($id_image > 0) {
+                            if (isset($this->context->smarty->tpl_vars['images']->value)) {
+                                $product_images = $this->context->smarty->tpl_vars['images']->value;
+                            }
+                            if (isset($product_images) && is_array($product_images) && isset($product_images[$id_image])) {
+                                $product_images[$id_image]['cover'] = 1;
+                                $this->context->smarty->assign('mainImage', $product_images[$id_image]);
+                                if (count($product_images)) {
+                                    $this->context->smarty->assign('images', $product_images);
+                                }
+                            }
+
+                            $cover = $current_cover;
+
+                            if (isset($cover) && is_array($cover) && isset($product_images) && is_array($product_images)) {
+                                $product_images[$cover['id_image']]['cover'] = 0;
+                                if (isset($product_images[$id_image])) {
+                                    $cover = $product_images[$id_image];
+                                }
+                                $cover['id_image'] = (Configuration::get('PS_LEGACY_IMAGES') ? ($this->product->id . '-' . $id_image) : (int) $id_image);
+                                $cover['id_image_only'] = (int) $id_image;
+                                $this->context->smarty->assign('cover', $cover);
+                            }
+                        }
+                    }
+                }
+            }
+
+            // wash attributes list depending on available attributes depending on selected preceding attributes
+            $current_selected_attributes = [];
+            $count = 0;
+            foreach ($groups as &$group) {
+                ++$count;
+                if ($count > 1) {
+                    //find attributes of current group, having a possible combination with current selected
+                    $id_product_attributes = [0];
+                    $query = 'SELECT pac.`id_product_attribute`
+                        FROM `' . _DB_PREFIX_ . 'product_attribute_combination` pac
+                        INNER JOIN `' . _DB_PREFIX_ . 'product_attribute` pa ON pa.id_product_attribute = pac.id_product_attribute
+                        WHERE id_product = ' . $this->product->id . ' AND id_attribute IN (' . implode(',', array_map('intval', $current_selected_attributes)) . ')
+                        GROUP BY id_product_attribute
+                        HAVING COUNT(id_product) = ' . count($current_selected_attributes);
+                    if ($results = Db::getInstance()->executeS($query)) {
+                        foreach ($results as $row) {
+                            $id_product_attributes[] = $row['id_product_attribute'];
+                        }
+                    }
+                    $id_attributes = Db::getInstance()->executeS('SELECT `id_attribute` FROM `' . _DB_PREFIX_ . 'product_attribute_combination` pac2
+                        WHERE `id_product_attribute` IN (' . implode(',', array_map('intval', $id_product_attributes)) . ')
+                        AND id_attribute NOT IN (' . implode(',', array_map('intval', $current_selected_attributes)) . ')');
+                    foreach ($id_attributes as $k => $row) {
+                        $id_attributes[$k] = (int) $row['id_attribute'];
+                    }
+                    foreach ($group['attributes'] as $key => $attribute) {
+                        if (!in_array((int) $key, $id_attributes)) {
+                            unset(
+                                $group['attributes'][$key],
+                                $group['attributes_quantity'][$key]
+                            );
+                        }
+                    }
+                }
+                //find selected attribute or first of group
+                $index = 0;
+                $current_selected_attribute = 0;
+                foreach ($group['attributes'] as $key => $attribute) {
+                    if ($index === 0) {
+                        $current_selected_attribute = $key;
+                    }
+                    if ($attribute['selected']) {
+                        $current_selected_attribute = $key;
+
+                        break;
+                    }
+                }
+                if ($current_selected_attribute > 0) {
+                    $current_selected_attributes[] = $current_selected_attribute;
+                }
+            }
+
+            // wash attributes list (if some attributes are unavailables and if allowed to wash it)
+            if (!Product::isAvailableWhenOutOfStock($this->product->out_of_stock) && Configuration::get('PS_DISP_UNAVAILABLE_ATTR') == 0) {
+                foreach ($groups as &$group) {
+                    foreach ($group['attributes_quantity'] as $key => &$quantity) {
+                        if ($quantity <= 0) {
+                            unset($group['attributes'][$key]);
+                        }
+                    }
+                }
+
+                foreach ($colors as $key => $color) {
+                    if ($color['attributes_quantity'] <= 0) {
+                        unset($colors[$key]);
+                    }
+                }
+            }
+            foreach ($this->combinations as $id_product_attribute => $comb) {
+                $attribute_list = '';
+                foreach ($comb['attributes'] as $id_attribute) {
+                    $attribute_list .= '\'' . (int) $id_attribute . '\',';
+                }
+                $attribute_list = rtrim($attribute_list, ',');
+                $this->combinations[$id_product_attribute]['list'] = $attribute_list;
+            }
+
+            return $groups;
+        } else {
+            return [];
+        }
     }
 }
