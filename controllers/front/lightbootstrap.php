ee955377ffa297c2e628d05e9f480b41cd281fbe
Sam <sam@binshops.com>
2022-02-16 14:28:55 -0600

    feat: adds currencies - languages - logo url to lightbootstrap

--- controllers/front/lightbootstrap.php
+++ controllers/front/lightbootstrap.php
@@ -13,10 +13,7 @@ require_once dirname(__FILE__) . '/../AbstractRESTController.php';
 require_once dirname(__FILE__) . '/../../classes/RESTMainMenu.php';
 require_once dirname(__FILE__) . '/../../classes/RESTProductLazyArray.php';
 
-use PrestaShop\PrestaShop\Adapter\Category\CategoryProductSearchProvider;
-use PrestaShop\PrestaShop\Core\Product\Search\ProductSearchContext;
-use PrestaShop\PrestaShop\Core\Product\Search\ProductSearchQuery;
-use PrestaShop\PrestaShop\Core\Product\Search\SortOrder;
+use PrestaShop\PrestaShop\Adapter\ObjectPresenter;
 
 /**
  * Description: This class bootstraps the main page of the application
@@ -59,8 +56,13 @@ class BinshopsrestLightbootstrapModuleFrontController extends AbstractRESTContro
             }
         }
 
+        $id_shop = (int) $this->context->shop->id;
+
         $psdata = array();
         $psdata['menuItems'] = $menuItems;
+        $psdata['currencies'] = $this->getCurrencies();
+        $psdata['languages'] = $this->getLanguages();
+        $psdata['logo_url'] = Tools::getHttpHost(true) . _PS_IMG_ .Configuration::get('PS_LOGO', null, null, $id_shop);
 
         $this->ajaxRender(json_encode([
             'success' => true,
@@ -95,5 +97,81 @@ class BinshopsrestLightbootstrapModuleFrontController extends AbstractRESTContro
             'message' => 'delete not supported on this path'
         ]));
         die;
+    }
+
+    protected function getCurrencies(){
+        $current_currency = null;
+        $serializer = new ObjectPresenter();
+        $currencies = array_map(
+            function ($currency) use ($serializer, &$current_currency) {
+                $currencyArray = $serializer->present($currency);
+
+                // serializer doesn't see 'sign' because it is not a regular
+                // ObjectModel field.
+                $currencyArray['sign'] = $currency->sign;
+
+                $url = $this->context->link->getLanguageLink($this->context->language->id);
+
+                $parsedUrl = parse_url($url);
+                $urlParams = [];
+                if (isset($parsedUrl['query'])) {
+                    parse_str($parsedUrl['query'], $urlParams);
+                }
+                $newParams = array_merge(
+                    $urlParams,
+                    [
+                        'SubmitCurrency' => 1,
+                        'id_currency' => $currency->id,
+                    ]
+                );
+                $newUrl = sprintf('%s://%s%s%s?%s',
+                    $parsedUrl['scheme'],
+                    $parsedUrl['host'],
+                    isset($parsedUrl['port']) ? ':' . $parsedUrl['port'] : '',
+                    $parsedUrl['path'],
+                    http_build_query($newParams)
+                );
+
+                $currencyArray['url'] = $newUrl;
+
+                if ($currency->id === $this->context->currency->id) {
+                    $currencyArray['current'] = true;
+                    $current_currency = $currencyArray;
+                } else {
+                    $currencyArray['current'] = false;
+                }
+
+                return $currencyArray;
+            },
+            Currency::getCurrencies(true, true)
+        );
+
+        return [
+            'currencies' => $currencies,
+            'current_currency' => $current_currency,
+        ];
+    }
+
+    protected function getLanguages(){
+        $languages = Language::getLanguages(true, $this->context->shop->id);
+
+        foreach ($languages as &$lang) {
+            $lang['name_simple'] = $this->getNameSimple($lang['name']);
+        }
+
+        return array(
+            'languages' => $languages,
+            'current_language' => array(
+                'id_lang' => $this->context->language->id,
+                'name' => $this->context->language->name,
+                'name_simple' => $this->getNameSimple($this->context->language->name),
+                'iso_code' => $this->context->language->iso_code
+            )
+        );
+    }
+
+    private function getNameSimple($name)
+    {
+        return preg_replace('/\s\(.*\)$/', '', $name);
     }
 }
