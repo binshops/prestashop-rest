12c8619c3361baf78f9e13e87cea44d17736626f
Sam <sam@binshops.com>
2023-01-08 09:54:25 -0500

    feat: refactoring REST responses

--- classes/RESTTrait.php
+++ classes/RESTTrait.php
@@ -11,54 +11,68 @@ trait RESTTrait
 
         parent::init();
 
+        $response = [
+            'success' => true,
+            'code' => 210,
+            'psdata' => null,
+            'message' => 'empty'
+        ];
+
         switch ($_SERVER['REQUEST_METHOD']) {
             case 'GET':
-                $this->processGetRequest();
+                $response = $this->processGetRequest();
                 break;
             case 'POST':
-                $this->processPostRequest();
+                $response = $this->processPostRequest();
                 break;
             case 'PATCH':
             case 'PUT':
-                $this->processPutRequest();
+                $response = $this->processPutRequest();
                 break;
             case 'DELETE':
-                $this->processDeleteRequest();
+                $response = $this->processDeleteRequest();
                 break;
             default:
                 // throw some error or whatever
         }
+
+        $this->ajaxRender(json_encode($response));
+        die;
     }
 
     protected function processGetRequest(){
-        $this->ajaxRender(json_encode([
+        return [
             'success' => true,
-            'message' => $this->trans('GET not supported on this path', [], 'Modules.Binshopsrest.Admin')
-        ]));
-        die;
+            'code' => 310,
+            'psdata' => null,
+            'message' => $this->trans('GET not supported on this path', [], 'Modules.Binshopsrest.Admin'),
+        ];
     }
 
     protected function processPostRequest(){
-        $this->ajaxRender(json_encode([
+        return [
             'success' => true,
-            'message' => $this->trans('POST not supported on this path', [], 'Modules.Binshopsrest.Admin')
-        ]));
-        die;
+            'code' => 310,
+            'psdata' => null,
+            'message' => $this->trans('POST not supported on this path', [], 'Modules.Binshopsrest.Admin'),
+        ];
     }
 
     protected function processPutRequest(){
-        $this->ajaxRender(json_encode([
+        return [
             'success' => true,
-            'message' => $this->trans('PUT not supported on this path', [], 'Modules.Binshopsrest.Admin')
-        ]));
-        die;
+            'code' => 310,
+            'psdata' => null,
+            'message' => $this->trans('PUT not supported on this path', [], 'Modules.Binshopsrest.Admin'),
+        ];
     }
 
     protected function processDeleteRequest(){
-        $this->ajaxRender(json_encode([
+        return [
             'success' => true,
-            'message' => $this->trans('DELETE not supported on this path', [], 'Modules.Binshopsrest.Admin')
-        ]));
-        die;
+            'code' => 310,
+            'psdata' => null,
+            'message' => $this->trans('DELETE not supported on this path', [], 'Modules.Binshopsrest.Admin'),
+        ];
     }
 }
