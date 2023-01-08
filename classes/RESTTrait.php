f57430e7b918af2457a4f2fc3a055821b039e790
Sam <sam@binshops.com>
2023-01-08 08:46:08 -0500

    feat: added ability to support REST within modules

--- classes/RESTTrait.php
+++ classes/RESTTrait.php
@@ -2,6 +2,34 @@
 
 trait RESTTrait
 {
+    public function restRun(){
+        header('Content-Type: ' . "application/json");
+        if (Tools::getValue('iso_currency')){
+            $_GET['id_currency'] = (string)Currency::getIdByIsoCode(Tools::getValue('currency'));
+            $_GET['SubmitCurrency'] = "1";
+        }
+
+        parent::init();
+
+        switch ($_SERVER['REQUEST_METHOD']) {
+            case 'GET':
+                $this->processGetRequest();
+                break;
+            case 'POST':
+                $this->processPostRequest();
+                break;
+            case 'PATCH':
+            case 'PUT':
+                $this->processPutRequest();
+                break;
+            case 'DELETE':
+                $this->processDeleteRequest();
+                break;
+            default:
+                // throw some error or whatever
+        }
+    }
+
     protected function processGetRequest(){
         $this->ajaxRender(json_encode([
             'success' => true,
